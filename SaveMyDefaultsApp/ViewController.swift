b0f38504c02a495dba5f49a5ef634805c13909dd
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-03-15 14:10:54 -0500

    works

--- SaveMyDefaultsApp/ViewController.swift
+++ SaveMyDefaultsApp/ViewController.swift
@@ -8,7 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func mySaveBtn(_ sender: Any) {
+        UserDefaults.standard.set(mySerialNum.text, forKey: "Text")
+        UserDefaults.standard.set(mySwitch.isOn, forKey: "Switch")
+        UserDefaults.standard.set(mySlider.value, forKey: "Slider")
+    }
+    
+    @IBAction func myLoadBtn(_ sender: Any) {
+        mySwitch.isOn = UserDefaults.standard.bool(forKey: "Switch")
+        mySlider.value = UserDefaults.standard.float(forKey: "Slider")
+        mySerialNum.text = UserDefaults.standard.string(forKey: "Text")
+    }
+    
+    @IBAction func myDeleteBtn(_ sender: Any) {
+        mySwitch.isOn = true
+        mySlider.value = 0.5
+        mySerialNum.text = ""
+    }
+    
+    @IBOutlet weak var mySerialNum: UITextField!
+    
+    @IBOutlet weak var mySlider: UISlider!
+    
+    @IBOutlet weak var mySwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
